9463963244550b211a2dbdc14cd86b55d37dbc87
Pirchio <45287349+Pirchio@users.noreply.github.com>
2018-11-23 08:07:19 -0300

    Update main.c
    Uliassi

--- ejercicio2/main.c
+++ ejercicio2/main.c
@@ -3,12 +3,20 @@
 
 int main(int argc, char* argv[])
 {
-
+    char* p;
+    char secuencia[150];
+    printf("ingrese la secuencia de palabras:\n");
+    gets(secuencia);
+    p = &secuencia;
 //ESCRIBA AQUI ARRIBA EL CÓDIGO DE ENTRADA SALIDA PARA OBTENER LOS DATOS SOLICITADOS EN EL ENUNCIADO
-
-	printf("\n#SALIDA#\n");
-	
 //A PARTIR DE ESTE PUNTO LA SALIDA DEL PROGRAMA DEBE COINCIDIR EXACTAMENTE CON EL LOTE DE PRUEBA
+	printf("\n#SALIDA#\n");
+	printf("%s\n", p);
+    while (p!='/0'){
+        if(*p == ',')
+            printf("%s\n",p+1);
+         p++;
+      }
 
 	return 0;
 }
