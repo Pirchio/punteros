7be58b09689da736b2087ef757ed5b87c6c34bf9
Pirchio <45287349+Pirchio@users.noreply.github.com>
2018-11-23 08:06:14 -0300

    Update intercambio.h

--- ejercicio1/intercambio.h
+++ ejercicio1/intercambio.h
@@ -4,4 +4,23 @@
 void intercambio(int* pi1, int* pi2);
 void ordenarEnteros(int viEnteros[], int iCantidad);
 
+
+void intercambio(int* pi1, int* pi2){
+    int aux;
+        aux = *pi1;
+        *pi1 = *pi2;
+        *pi2 = aux;
+}
 #endif
+void ordenarEnteros(int viEnteros[], int iCantidad){
+    int l = 1, i;
+	while(l==1){
+		l = 0;
+		for(i = 0; i < iCantidad-1;i++){
+			if(viEnteros[i] > viEnteros[i+1]){
+				intercambio(&viEnteros[i], &viEnteros[i+1]);
+			}
+		}
+	}
+}
+
